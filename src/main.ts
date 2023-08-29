8559f0b3feaf2ae6c68b6a2a6beff2bdf6374f87
ars-4 <ars7.dev@gmail.com>
2023-08-30 00:41:38 +0500

    Backend URL

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import '@/assets/css/icons.css';
 
 const app = createApp(App);
 
-app.config.globalProperties.$api = process.env.API_BACKEND;
+app.config.globalProperties.$api = "https://redesign-estore-backend.ars-4.repl.co/api";
 
 app.config.globalProperties.$auth = true;
 
